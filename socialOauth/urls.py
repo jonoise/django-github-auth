52924f39590d16272c55b7587561286af701c063
Amilkar Munoz <yellowballpython@gmail.com>
2021-06-15 00:11:58 -0600

    working with cookies

--- socialOauth/urls.py
+++ socialOauth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CallbackAPIView
+from .views import CallbackAPIView, TestView
 app_name = "social"
 
 urlpatterns = [
     # path('register/github/', GithubRegisterAPI.as_view(), name="github-register"),
     # path('frontend', FrontEndHandler.as_view(), name="frontend-request"),
+    path('test/', TestView.as_view(), name="test"),
     path('callback/github/', CallbackAPIView.as_view(), name="callback"),
 ]
--- socialOauth/views.py
+++ socialOauth/views.py
@@ -1,8 +1,10 @@
 import asyncio
+from rest_framework.response import Response
 
 from .github import Github
 from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
+from rest_framework import permissions
+from django.http import HttpResponseRedirect
 
 
 class GithubRegisterAPI(GenericAPIView):
@@ -20,8 +22,29 @@ class CallbackAPIView(GenericAPIView):
         data = asyncio.run(Github.request_access_token(code))
         access_token = data.get('access_token')
         user_email = asyncio.run(Github.get_user_email(access_token))
-
         user = Github.register_or_authenticate(
             user_email=user_email, access_token=access_token)
+        tokens = user.get('tokens')
+        refresh = tokens['refresh']
+        access = tokens['access']
 
-        return Response(data=user)
+        # redirect
+        REDIRECT_URL = 'http://localhost:3000/test'
+        response = HttpResponseRedirect(REDIRECT_URL)
+        response.set_cookie("refresh", refresh)
+        response.set_cookie("access", access)
+        return response
+
+
+class TestView(GenericAPIView):
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        response = Response({'oh': f'you are authenticated {user.email}'})
+        response['Access-Control-Allow-Credentials'] = 'true'
+        response['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        response['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, PATCH, OPTIONS'
+        return response
